e783a46dcbb6e50f5bbde3c704ec6d8b340cb0f1
mohammad-a-hossain <adam.eve007@gmail.com>
2021-12-11 01:42:24 +0600

    a new project for redux

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './styles/App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className='text-center text-primary'>h3ll o </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
